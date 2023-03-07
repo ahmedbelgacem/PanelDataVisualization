82dbc89da15c7e01c2d82c6b2d01f82a9424d468
Ahmed Belgacem <ahmedbelgaacem@gmail.com>
2023-03-07 11:04:57 +0100

    added total score column to the dataframe

--- dashboard.py
+++ dashboard.py
@@ -5,12 +5,14 @@ import pandas as pd
 
 pn.extension(sizing_mode = 'stretch_width')
 vanilla = pn.template.VanillaTemplate(title='Vanilla Template')
+df_data = pd.read_csv("data/StudentsPerformance.csv")
+df_data['total score (%)'] = round((df_data['math score']+df_data['reading score']+df_data['writing score']) / 3, 1)
+table = hv.Table(df_data)
 
 xs = np.linspace(0, np.pi)
 freq = pn.widgets.FloatSlider(name="Frequency", start=0, end=10, value=2)
 phase = pn.widgets.FloatSlider(name="Phase", start=0, end=np.pi)
-df_data = pd.read_csv("data/StudentsPerformance.csv")
-table = hv.Table(df_data)
+
 @pn.depends(freq=freq, phase=phase)
 def sine(freq, phase):
     return hv.Curve((xs, np.sin(xs*freq+phase))).opts(
