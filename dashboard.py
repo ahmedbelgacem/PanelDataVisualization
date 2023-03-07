a7297bea73cf1a560f6a9b2cd69ce75968219f98
Ahmed Belgacem <ahmedbelgaacem@gmail.com>
2023-03-07 12:01:57 +0100

    added summary of the scores with average, min and max

--- dashboard.py
+++ dashboard.py
@@ -8,14 +8,21 @@ pn.extension(sizing_mode = 'stretch_width')
 vanilla = pn.template.VanillaTemplate(title='Vanilla Template')
 df_data = pd.read_csv("data/StudentsPerformance.csv")
 df_data['total score (%)'] = round((df_data['math score']+df_data['reading score']+df_data['writing score']) / 3, 1)
-table = hv.Table(df_data)
+vanilla.main.append(Table(df_data))
+
+summary = df_data.describe().T[["min","mean","75%","max"]]\
+.rename(columns={"min": "minimum_score",
+                "mean": "average_score",
+                 "75%":"75% of scores less",
+                "max": "maximum_score"}).astype(int)
+index = ["math score", "reading score", "writing score", "total score (%)"]
+summary_df = pd.DataFrame(summary, index=index)
+vanilla.main.append(Table(summary_df))
 
 xs = np.linspace(0, np.pi)
 freq = pn.widgets.FloatSlider(name="Frequency", start=0, end=10, value=2)
 phase = pn.widgets.FloatSlider(name="Phase", start=0, end=np.pi)
-df_data = pd.read_csv('data/StudentsPerformance.csv')
-df_data['total score (%)'] = round((df_data['math score']+df_data['reading score']+df_data['writing score']) / 3, 1)
-vanilla.main.append(Table(df_data))
+
 
 @pn.depends(freq=freq, phase=phase)
 def sine(freq, phase):
