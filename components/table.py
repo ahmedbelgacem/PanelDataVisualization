f0c52181453f7d27205a4d07dd6825b7443f41ea
Ahmed Belgacem <ahmedbelgaacem@gmail.com>
2023-03-07 17:13:02 +0100

    added filter for the table

--- components/table.py
+++ components/table.py
@@ -2,19 +2,28 @@ import panel as pn
 from panel.widgets import Tabulator
 import pandas as pd
 
-def Table(data : pd.DataFrame, index: list = None, columns: list = None):
-  if index:
-    data = data.set_index(pd.Index(index))
-  data.insert(0, '', index)
 
-  if columns and len(columns) != len(data.columns) - 1:
-    raise ValueError('columns length is {}, while data has {} columns'.format(len(columns), len(data.columns) - 1))
-  if columns: columns = [''] + columns
+class Table():
+    def __init__(self, data: pd.DataFrame, index: list = None, columns: list = None):
+        self.data = data
+        self.index = index
+        self.columns = columns
+        # if self.index is not None:
+        #     self.data = self.data.set_index(pd.Index(self.index))
+        # self.data.insert(0, '', index)
+        #
+        # if self.columns is not None and len(columns) != len(data.columns) - 1:
+        #     raise ValueError(
+        #         'columns length is {}, while data has {} columns'.format(len(columns), len(data.columns) - 1))
+        # if self.columns: columns = [''] + columns
 
-  return Tabulator(
-    data, 
-    layout = 'fit_data_fill', 
-    page_size = 10, 
-    show_index = False,
-    titles = {data.columns[i]: (columns[i] if columns else data.columns[i]) for i in range(len(data.columns))},
-  )
+        self.figure = Tabulator(
+            data,
+            layout='fit_data_fill',
+            page_size=10,
+            show_index=False,
+            #titles={data.columns[i]: (columns[i] if columns else data.columns[i]) for i in range(len(data.columns))},
+        )
+
+    def update(self, df):
+        self.figure.value = df
--- dashboard.py
+++ dashboard.py
@@ -6,23 +6,30 @@ from components.table import Table
 from components.Heatmap import Heatmap
 from config import DATA_PATH
 
-pn.extension(sizing_mode = 'stretch_width')
-vanilla = pn.template.VanillaTemplate(title = 'Vanilla Template')
+pn.extension(sizing_mode='stretch_width')
+vanilla = pn.template.VanillaTemplate(title='Vanilla Template')
 pn.widgets.Tabulator.theme = 'materialize'
 
 df_data = pd.read_csv(DATA_PATH)
 df_data['total score (%)'] = round((df_data['math score'] + df_data['reading score'] + df_data['writing score']) / 3, 1)
-vanilla.main.append(Table(df_data, index = range(len(df_data)), columns = ['Gender', 'Race/Ethnicity', 'Parental level education', 'Lunch', 'Test prep. course', 'Math score (%)', 'Reading score (%)', 'Writing score (%)', 'Total score (%)']))
 
-summary = df_data.describe().T[['min', 'mean', '75%','max']].astype(int)
+table_df = Table(df_data, index=range(len(df_data)),
+                 columns=['Gender', 'Race/Ethnicity', 'Parental level education', 'Lunch', 'Test prep. course',
+                          'Math score (%)', 'Reading score (%)', 'Writing score (%)', 'Total score (%)'])
+vanilla.main.append(table_df.figure)
+
+summary = df_data.describe().T[['min', 'mean', '75%', 'max']].astype(int)
 index = ['math score', 'reading score', 'writing score', 'total score (%)']
-summary_df = pd.DataFrame(summary, index = index)
+summary_df = pd.DataFrame(summary, index=index)
+table_summary = Table(summary_df, index, columns=['Min. score', 'Avg. score', 'Q3', 'Max. score'])
 vanilla.main.append(
     pn.Row(
-        pn.Column(Table(summary_df, index, columns = ['Min. score', 'Avg. score', 'Q3', 'Max. score'])),
-        pn.Column(Table(summary_df, index, columns = ['Min. score', 'Avg. score', 'Q3', 'Max. score']))
+        pn.Column(table_summary.figure),
+        pn.Column(table_summary.figure)
     )
 )
+
+select = pn.widgets.Select(name="Select", options=['A', 'B', 'C', 'D', 'E'])
 
 xs = np.linspace(0, np.pi)
 freq = pn.widgets.FloatSlider(name='Frequency', start=0, end=10, value=2)
@@ -33,18 +40,28 @@ vanilla.main.append(
         pn.Card(Heatmap(df_data), title='Feature Correlation'),
     )
 )
+
+
 @pn.depends(freq=freq, phase=phase)
 def sine(freq, phase):
-    return hv.Curve((xs, np.sin(xs*freq+phase))).opts(
+    return hv.Curve((xs, np.sin(xs * freq + phase))).opts(
         responsive=True, min_height=400)
+
 
 @pn.depends(freq=freq, phase=phase)
 def cosine(freq, phase):
-    return hv.Curve((xs, np.cos(xs*freq+phase))).opts(
+    return hv.Curve((xs, np.cos(xs * freq + phase))).opts(
         responsive=True, min_height=400)
+
+
+@pn.depends(select=select, watch=True)
+def filter(select):
+    df_new = df_data[df_data['race/ethnicity'] == f"group {select}"]
+    table_df.update(df_new)
 
 vanilla.sidebar.append(freq)
 vanilla.sidebar.append(phase)
+vanilla.sidebar.append(select)
 vanilla.main.append(
     pn.Row(
         pn.Card(hv.DynamicMap(sine), title='Sine'),
