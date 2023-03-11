f5bd9a8774edfeab94dedcbc02dea5ce16def967
nizar-masmoudi <nizarmasmoudi@outlook.fr>
2023-03-11 17:09:38 +0100

    Setup home and ai pages

--- ai.py
+++ ai.py
@@ -2,8 +2,8 @@ import panel as pn
 from utils.utils import read_csv
 
 pn.extension(sizing_mode = 'stretch_width')
-with open('templates/home.jinja2', 'r') as html:
+with open('templates/ai.jinja2', 'r') as html:
   template = pn.Template('\n'.join(html.readlines()))
-template.add_variable('app_title', 'Home')
+template.add_variable('app_title', 'Machine Learning')
 
 template.servable()
